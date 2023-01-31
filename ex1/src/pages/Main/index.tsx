13fa72c86f54c4f7970bd5ab55f855253db19e07
Andre Luca <lucaprojob@gmail.com>
2023-01-31 05:37:30 -0300

    change: initial ex1 setup

---
+++ ex1/src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './style.css'
+
+function Main() {
+
+    return (
+        <div className="App">
+            <h1>Main</h1>
+        </div>
+    )
+}
+
+export default Main
