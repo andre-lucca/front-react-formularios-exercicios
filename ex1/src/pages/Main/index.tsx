76caf3eef66f7c68955093e0870ab74ab70952c0
Andre Luca <lucaprojob@gmail.com>
2023-02-02 06:32:59 -0300

    Exercício feito

--- ex1/src/pages/Main/index.tsx
+++ ex1/src/pages/Main/index.tsx
@@ -1,18 +1,45 @@
-import { useState } from 'react'
+import { BaseSyntheticEvent, SyntheticEvent, useState } from 'react'
 import './style.css'
 
 import openEyeImg from '../../assets/open-eye.svg'
 import closeEyeImg from '../../assets/close-eye.svg'
 
 function Main() {
+  const [formState, setFormState] = useState({})
+  const [isPwVisible, setPassWordVisibility] = useState(false)
+
+  function handleSubmit(event: SyntheticEvent) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const [emailInput, passwordInput] = form.querySelectorAll('input')
+
+    if (!emailInput.value || !passwordInput.value) {
+      console.log('Os campos Email e Password precisam estar preenchidos.')
+    }
+
+    console.log('Sucesso!')
+  }
 
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Login</h1>
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button>Login</button>
+        <input
+          type="email"
+          placeholder="E-mail"
+        />
+        <div className="password-input-container">
+          <input
+            type={isPwVisible ? "text" : "password"}
+            placeholder="Senha"
+          />
+          <img
+            src={isPwVisible ? openEyeImg : closeEyeImg}
+            alt="Password visibility"
+            onClick={() => setPassWordVisibility(!isPwVisible)}
+          />
+        </div>
+        <button type="submit">Login</button>
       </form>
     </div >
   )
