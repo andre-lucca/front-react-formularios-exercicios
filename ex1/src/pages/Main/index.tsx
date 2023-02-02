b88032f8efcd12c4326c611b175cf7d4b4c932a1
Andre Luca <lucaprojob@gmail.com>
2023-02-02 05:20:38 -0300

    stryling: css feito.

--- ex1/src/pages/Main/index.tsx
+++ ex1/src/pages/Main/index.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './style.css'
 
+import openEyeImg from '../../assets/open-eye.svg'
+import closeEyeImg from '../../assets/close-eye.svg'
+
 function Main() {
 
-    return (
-        <div className="App">
-            <h1>Main</h1>
-        </div>
-    )
+  return (
+    <div className="container">
+      <form>
+        <h1>Login</h1>
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <button>Login</button>
+      </form>
+    </div >
+  )
 }
 
 export default Main
